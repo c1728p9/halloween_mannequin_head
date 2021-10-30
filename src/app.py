cc944ab4f3c1a9dee7e91a1994554ac1429ef94e
Russ <butleja10511@hotmail.com>
2021-10-30 14:04:46 -0500

    Show box on found image

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import threading
 import time
 import requests
 import subprocess
+import cv2
 from rtsparty import Stream
 from objectdaddy import Daddy
 
@@ -26,7 +27,8 @@ class HalloweenMannequinHead():
     def _setup_stream(self):
         """Set up the stream to the camera"""
         logging.info('Starting stream')
-        self.stream = Stream(os.environ.get('STREAM_URI', None))
+        #self.stream = Stream(os.environ.get('STREAM_URI', None))
+        self.stream = Stream()
 
     def _setup_object_recognition(self):
         """Set up object recognition and load models"""
@@ -52,17 +54,33 @@ class HalloweenMannequinHead():
 
     def process_frames_from_stream(self):
         """Processes the frames from the stream"""
+        frame_num = 0
         while True:
             frame = self.stream.get_frame()
+            #print("Frame type: %s", type(frame))
+            print("Frame")
             if self.stream.is_frame_empty(frame):
                 continue
+            #cv2.imwrite('./image-%s.png' % frame_num, frame)
+            #cv2.imshow('Camera',frame)
+            #cv2.waitKey(0)
             self.latest_frame = frame
             results, frame = self.daddy.process_frame(frame)
             for detection in results:
                 if detection.is_person():
                     self.person_detected(detection)
+                    start_point = (detection.x, detection.y)
+                    end_point = (detection.x + detection.w, detection.y + detection.h)
+                    color = (255, 0, 0)
+                    thickness = 2
+                    cv2.rectangle(frame, start_point, end_point, color, thickness)
+                    cv2.imwrite('./image-%s.png' % frame_num, frame)
+                    cv2.imshow('Camera',frame)
+                    cv2.waitKey(0)
+                    frame_num += 1
             if self.server_mode:
                 time.sleep(0.1)
+
 
     def run(self):
         """Run the application"""
